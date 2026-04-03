7245ff979786408ab75d787ae6dfb62f8c57eefb
Pratham Thakuran <prathamthakuran04@gmail.com>
2026-04-03 20:51:31 +0530

    abstarctcode.java

---
+++ abstractcode.java
@@ -0,0 +1,14 @@
+abstract class Animal {
+    abstract void sound();
+}
+class Dog extends Animal {
+    void sound() {
+        System.out.println("Dog barks");
+    }
+}
+class abstractcode {
+    public static void main(String[] args) {
+        Animal a = new Dog();
+        a.sound();
+    }
+}
