fea6ce4d4ed05ca8a89608035131028a9fe8319e
pratham-thakuran <Pratham.11862@stu.upes.ac.in>
2026-04-11 23:39:29 +0530

    updated mainBook logic

--- mainBook.java
+++ mainBook.java
@@ -128,11 +128,5 @@ public class mainBook{
 		System.out.println("Updated now will learn and apply more things on this");
 		System.out.println("Updated now will learn and apply more things on this");
 		System.out.println("Updated now will learn and apply more things on this");
-		System.out.println("Updated now will learn and apply more things on this");
-		System.out.println("Updated now will learn and apply more things on this");
-		System.out.println("Updated now will learn and apply more things on this");
-		System.out.println("Updated now will learn and apply more things on this");
-		System.out.println("Updated now will learn and apply more things on this");
-
 	}
 }
