25af3aa9ca3236c6754e367f92a612208ad6f995
pratham-thakuran <Pratham.11862@stu.upes.ac.in>
2026-04-22 23:44:23 +0530

    Nothing updated

--- mainBook.java
+++ mainBook.java
@@ -242,8 +242,7 @@ public class mainBook{
 		else{
 			System.out.println("Invalid input!!");
 		}
-		System.out.println("Updated now will learn and apply more things on this");
-		System.out.println("Nothing!!");
+		System.out.println("Updated now will learn and apply more things on this")
 
 	}
 }
