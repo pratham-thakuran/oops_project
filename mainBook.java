898dc88144b144ecea8a326d80efa1501d754b4d
pratham-thakuran <Pratham.11862@stu.upes.ac.in>
2026-05-05 23:05:06 +0530

    sorry

--- mainBook.java
+++ mainBook.java
@@ -278,7 +278,6 @@ public class mainBook{
 			System.out.println("Invalid input!!");
 		}
 		System.out.println("Updated now will learn and apply more things on this");
-		System.out.println("Updated now will learn and apply more things on this");
 
 	}
 }
